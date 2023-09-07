c1ef1a9dca45b1ac28660c5bd3d13b09a34aaf13
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-09-07 16:35:18 -0300

    Adicionar arquivos de projeto.

---
+++ Musica.cs
@@ -0,0 +1,23 @@
+﻿class Musica {
+    public string nome;
+    private bool disponivel;
+    public string artista;
+    public int duracao;
+
+
+    public void ExibirFichaTecnica() {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel) {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else {
+            Console.WriteLine("Adiquira o plano Plus+");
+        }
+    }
+
+    public void ExibirNomeArtista() {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+    }
+}
